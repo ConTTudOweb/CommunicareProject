2dc21b8defc155f915d0b0df3bf34e7b57be974c
Alessandro de Lima Folk <sandrofolk@hotmail.com>
2019-01-09 15:05:51 -0200

    Validação do form

--- communicare/core/views.py
+++ communicare/core/views.py
@@ -16,30 +16,36 @@ class HomeView(TemplateView):
 
 
 def contact(request):
+    response_data = {}
     if request.method == 'POST':
-        response_data = {}
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # send email using the request.POST dictionary
+            to = request.POST.get('email')
+            from_ = str(settings.DEFAULT_FROM_EMAIL),
 
-        # send email using the request.POST dictionary
-        to = request.POST.get('email')
-        from_ = str(settings.DEFAULT_FROM_EMAIL),
+            message = "{name} / {email} / {phone} disse: ".format(
+                name=request.POST.get('name'),
+                email=to,
+                phone=request.POST.get('phone'),
+            )
+            message += "\n\n{0}".format(request.POST.get('message'))
+            email = mail.EmailMessage(
+                subject='Contato pelo site.',
+                body=message,
+                to=from_,
+                reply_to=[to],
+            )
+            email.send()
+            response_data['result'] = 'Obrigado pelo seu contato!<br>' \
+                                      'O interesse pelo curso foi cadastrado com sucesso, ' \
+                                      'retornaremos o mais rápido possível.'
 
-        message = "{name} / {email} / {phone} disse: ".format(
-            name=request.POST.get('name'),
-            email=to,
-            phone=request.POST.get('phone'),
-        )
-        message += "\n\n{0}".format(request.POST.get('message'))
-        email = mail.EmailMessage(
-            subject='Contato pelo site.',
-            body=message,
-            to=from_,
-            reply_to=[to],
-        )
-        email.send()
-        response_data['result'] = 'Obrigado pelo seu contato!<br>' \
-                                  'O interesse pelo curso foi cadastrado com sucesso, ' \
-                                  'retornaremos o mais rápido possível.'
-
-        return JsonResponse(response_data)
+            return JsonResponse(response_data)
+        else:
+            errors = ""
+            for error in form.non_field_errors():
+                errors += error
+            return JsonResponse({'result': errors})
     else:
         return JsonResponse({"nothing to see": "this isn't happening"})
