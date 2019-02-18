0870615fcb338ee37da475878dea5b2b59e80659
Alessandro de Lima Folk <sandrofolk@hotmail.com>
2019-02-18 11:22:54 -0300

    Colocar site no AR

--- communicare/settings.py
+++ communicare/settings.py
@@ -211,5 +211,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 cloudinary.config(
     cloud_name=config('CLOUDINARY_CLOUD_NAME'),
     api_key=config('CLOUDINARY_API_KEY'),
-    api_secret=config('CLOUDINARY_API_SECRET')
+    api_secret=config('CLOUDINARY_API_SECRET'),
+    secure=True
 )
